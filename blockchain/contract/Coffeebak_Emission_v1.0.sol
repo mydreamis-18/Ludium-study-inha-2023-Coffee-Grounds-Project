716104f86bd510bfa371dfd2d0fd28d864b10986
mydreamis-18 <hitoryok@naver.com>
2023-11-11 09:11:56 +0900

    블록체인 배출 컨트랙에서 ict 토큰 컨트랙 상호작용 에러

--- blockchain/contract/Coffeebak_Emission_v1.0.sol
+++ blockchain/contract/Coffeebak_Emission_v1.0.sol
@@ -5,9 +5,9 @@ import "./Incheon_Coffeebak_Token_flattened_v2.0.sol";
 
 contract Coffeebak_Emission is Ownable {
     //
-    Incheon_Coffeebak_Token private _ICT_token;
     uint256 public total_emission_count;
     uint256 public total_emission_amount;
+    Incheon_Coffeebak_Token private _token;
 
     // 커피박 1 kg당 인센티브로 지급해주는 ICT 토큰 단위
     // 현재로서는 1 kg당 30 ICT 지급 예정
@@ -15,16 +15,15 @@ contract Coffeebak_Emission is Ownable {
 
     // 카페의 지갑 주소를 바탕으로 해당 배출 데이터를 배열로 저장한다.
     struct emission_data {
-        uint256 verification_date;
-        uint256 coffeebak_amount;
-        string collector_name;
-        uint256 token_amount;
         string cafe_name;
+        string collector_name;
+        string verification_date;
+        uint256 coffeebak_amount;
     }
 
     // 매핑 객체 cafe_emission_data에는 커피박을 배출한 모든 카페의 지갑 주소가 담겨 있고
     // 각 카페별로 배출 데이터의 배열이 담겨 있다.
-    mapping(address cafe_wallet => emission_data[]) public cafe_emission_data;
+    mapping(address => emission_data[]) public cafe_emission_data;
 
     // 여기서 msg.sender는 배포자
     // 오로지 배포자만이 오너가 될 수 있다.
@@ -33,102 +32,153 @@ contract Coffeebak_Emission is Ownable {
         uint256 _coffeebak_insentive_unit
     ) Ownable(_msgSender()) {
         //
-        _ICT_token = _ICT_CA;
+        _token = _ICT_CA;
         coffeebak_insentive_unit = _coffeebak_insentive_unit;
     }
 
-    function get_cafe_emission_data(
-        address _cafe_wallet
-    ) public view returns (emission_data[] memory) {
-        //
-        return cafe_emission_data[_cafe_wallet];
-    }
+    // function get_cafe_emission_data(
+    //     address _cafe_wallet
+    // ) public view returns (emission_data[] memory) {
+    //     //
+    //     return cafe_emission_data[_cafe_wallet];
+    // }
 
-    function get_cafe_total_emission_count(
-        address _cafe_wallet
-    ) public view returns (uint256) {
-        //
-        return cafe_emission_data[_cafe_wallet].length;
-    }
+    // function get_cafe_total_emission_count(
+    //     address _cafe_wallet
+    // ) public view returns (uint256) {
+    //     //
+    //     return cafe_emission_data[_cafe_wallet].length;
+    // }
 
-    function get_cafe_total_emission_amount(
-        address _cafe_wallet
-    ) public view returns (uint256) {
-        //
-        uint256 cafe_total_emission_amount;
-        emission_data[] memory the_cafe_emission_data = cafe_emission_data[
-            _cafe_wallet
-        ];
+    // function get_cafe_total_emission_amount(
+    //     address _cafe_wallet
+    // ) public view returns (uint256) {
+    //     //
+    //     uint256 cafe_total_emission_amount;
+    //     emission_data[] memory the_cafe_emission_data = cafe_emission_data[
+    //         _cafe_wallet
+    //     ];
 
-        for (uint256 i = 0; i < the_cafe_emission_data.length; i++) {
-            cafe_total_emission_amount += the_cafe_emission_data[i].coffeebak_amount;
-        }
-        return cafe_total_emission_amount;
-    }
+    //     for (uint256 i = 0; i < the_cafe_emission_data.length; i++) {
+    //         cafe_total_emission_amount += the_cafe_emission_data[i]
+    //             .coffeebak_amount;
+    //     }
+    //     return cafe_total_emission_amount;
+    // }
 
-    function set_coffeebak_insentive_unit(
-        uint256 _new_coffeebak_insentive_unit
-    ) public onlyOwner {
-        //
-        coffeebak_insentive_unit = _new_coffeebak_insentive_unit;
-    }
+    // function set_coffeebak_insentive_unit(
+    //     uint256 _new_coffeebak_insentive_unit
+    // ) public onlyOwner {
+    //     //
+    //     coffeebak_insentive_unit = _new_coffeebak_insentive_unit;
+    // }
 
-    // 배출 데이터를 저장함과 동시에 토큰을 전송해준다.
-    // 오너만 호출할 수 있다.
-    function add_cafe_emission_data_list(
-        emission_data[] memory _emission_data_list,
-        address[] memory _cafe_wallet
-    ) public onlyOwner returns (bool) {
-        //
-        for (uint256 i = 0; i < _emission_data_list.length; i++) {
-            emission_data memory the_emission_data = _emission_data_list[i];
-            add_cafe_emission_data(
-                the_emission_data.collector_name,
-                the_emission_data.verification_date,
-                the_emission_data.coffeebak_amount,
-                the_emission_data.cafe_name,
-                the_emission_data.token_amount,
-                _cafe_wallet[i]
-            );
-        }
-        return true;
-    }
+    // // 배출 데이터를 저장함과 동시에 토큰을 전송해준다.
+    // // 오너만 호출할 수 있다.
+    // function add_cafe_emission_data_list(
+    //     emission_data[] memory _emission_data_list,
+    //     address[] memory _cafe_wallet,
+    //     uint256[] memory deadline,
+    //     uint8[] memory v,
+    //     bytes32[] memory r,
+    //     bytes32[] memory s
+    // ) public onlyOwner returns (bool) {
+    //     //
+    //     for (uint256 i = 0; i < _emission_data_list.length; i++) {
+    //         emission_data memory the_emission_data = _emission_data_list[i];
+    //         add_cafe_emission_data(
+    //             _cafe_wallet[i],
+    //             the_emission_data.cafe_name,
+    //             the_emission_data.collector_name,
+    //             the_emission_data.verification_date,
+    //             the_emission_data.coffeebak_amount,
+    //             deadline[i],
+    //             v[i],
+    //             r[i],
+    //             s[i]
+    //         );
+    //     }
+    //     return true;
+    // }
 
-    // 배출 데이터를 저장함과 동시에 토큰을 전송해준다.
-    // 오너만 호출할 수 있다.
-    function add_cafe_emission_data(
-        string memory _collector_name,
-        uint256 _verification_date,
-        uint256 _coffeebak_amount,
-        string memory _cafe_name,
+    // // 배출 데이터를 저장함과 동시에 토큰을 전송해준다.
+    // // 오너만 호출할 수 있다.
+    // function add_cafe_emission_data(
+    //     address _cafe_wallet,
+    //     string memory _cafe_name,
+    //     string memory _collector_name,
+    //     string memory _verification_date,
+    //     uint256 _coffeebak_amount,
+    //     uint256 deadline,
+    //     uint8 v,
+    //     bytes32 r,
+    //     bytes32 s
+    // ) public onlyOwner returns (bool) {
+    //     //
+    //     emission_data memory new_emission_data = emission_data(
+    //         _cafe_name,
+    //         _collector_name,
+    //         _verification_date,
+    //         _coffeebak_amount
+    //     );
+
+    //     total_emission_count++;
+    //     total_emission_amount += _coffeebak_amount;
+    //     cafe_emission_data[_cafe_wallet].push(new_emission_data);
+
+    //     transfer_token(
+    //         _cafe_wallet,
+    //         _coffeebak_amount * coffeebak_insentive_unit,
+    //         deadline,
+    //         v,
+    //         r,
+    //         s
+    //     );
+    //     return true;
+    // }
+
+    // // 커피박 배출 후 검증이 완료되면 배출 양에 따라
+    // // ICT CA에서 카페 지갑으로 ICT 토큰을 전송해준다.
+    // function transfer_token(
+    //     address _to,
+    //     uint256 _token_amount,
+    //     uint256 deadline,
+    //     uint8 v,
+    //     bytes32 r,
+    //     bytes32 s
+    // ) internal onlyOwner {
+    //     //
+    //     // permit_token(_token_amount, deadline, v, r, s);
+
+    //     // IERC20 ict_token = IERC20(_ICT_CA);
+    //     // ict_token.transferFrom(_ICT_CA, _to, _token_amount);
+    // }
+
+    // function permit_token(
+    //     uint256 _token_amount,
+    //     uint256 deadline,
+    //     uint8 v,
+    //     bytes32 r,
+    //     bytes32 s
+    // ) public onlyOwner {
+    //     //
+    //     // IERC20Permit ict_token = IERC20Permit(_ICT_CA);
+    //     // ict_token.permit(
+    //     //     _ICT_CA,
+    //     //     address(this),
+    //     //     _token_amount,
+    //     //     deadline,
+    //     //     v,
+    //     //     r,
+    //     //     s
+    //     // );
+    // }
+
+    function transfer_test(
         uint256 _token_amount,
-        address _cafe_wallet
-    ) public onlyOwner returns (bool) {
+        address _to
+    ) public {
         //
-        emission_data memory new_emission_data = emission_data(
-            _verification_date,
-            _coffeebak_amount,
-            _collector_name,
-            _token_amount,
-            _cafe_name
-        );
-
-        total_emission_count++;
-        total_emission_amount += _coffeebak_amount;
-        cafe_emission_data[_cafe_wallet].push(new_emission_data);
-
-        transfer_token(
-            _cafe_wallet,
-            _coffeebak_amount * coffeebak_insentive_unit
-        );
-        return true;
-    }
-
-    // 여기서 address(this)는 커피박 에미션의 CA
-    // 커피박 배출 후 검증이 완료되면 배출 양에 따라
-    // 커피박 에미션 CA에서 카페 지갑으로 ICT 토큰을 전송해준다.
-    function transfer_token(address _to, uint256 _token_amount) internal {
-        //
-        _ICT_token.transferFrom(address(this), _to, _token_amount);
+        _token.transfer(_to, _token_amount);
     }
 }
--- blockchain/interact/interact_Coffeebak_Emission_v1.0.js
+++ blockchain/interact/interact_Coffeebak_Emission_v1.0.js
@@ -27,7 +27,7 @@ const { get_infura_provider_fn } = require("../create_wallet_ethers_v5.7.2");
 
 (async () => {
     //
-    const fileName = "Coffeebak_Emission_flattened_v1.1";
+    const fileName = "Coffeebak_Emission_v1.0";
 
 
     const api_key = process.env.JIWON_API_KEY;
@@ -63,7 +63,7 @@ const { get_infura_provider_fn } = require("../create_wallet_ethers_v5.7.2");
 
     const ict_ca = "0x279609ED694c06452992b826C7F4750efE3a4280";
     const owner = ict_ca;
-    const spender = ca;
+    const spender = "0x7d547B43a6514ff4470746312AA87e7cDedB3dF2";
     const value = 30;
     const nonce = await ict_read_only_contract.nonces(owner);
     const deadline = Math.floor(new Date().getTime() / 1000) + 60 * 60 * 24;
@@ -78,20 +78,25 @@ const { get_infura_provider_fn } = require("../create_wallet_ethers_v5.7.2");
     }
 
 
-    const signature = await wallet._signTypedData(domain, { Permit: types }, values);
-    const { v, r, s } = ethers.utils.splitSignature(signature);
+    // const signature = await wallet._signTypedData(domain, { Permit: types }, values);
+    // const { v, r, s } = ethers.utils.splitSignature(signature);
     // const tx = await contract.permit(owner, spender, value, deadline, v, r, s, { gasLimit: 1000000 });
 
 
-    console.log(deadline);
-    console.log(v);
-    console.log(r);
-    console.log(s);
+    // console.log(deadline);
+    // console.log(v);
+    // console.log(r);
+    // console.log(s);
 
 
-    const cafe_wallet = "0x7d547B43a6514ff4470746312AA87e7cDedB3dF2";
+    // const cafe_wallet = "0x7d547B43a6514ff4470746312AA87e7cDedB3dF2";
     // const tx = await contract.add_cafe_emission_data(cafe_wallet, "지원이네", "지원", "2023-10-30", 100, deadline, v, r, s, { gasLimit: 100000 })
-    const tx = await contract.transfer_token(cafe_wallet, 30, deadline, v, r, s, { gasLimit: 100000 })
+    // const tx = await contract.permit_token(30, deadline, v, r, s, { gasLimit: 100000 })
+
+
+    // 가스비 에러 발생
+    // 'cannot estimate gas; transaction may fail or may require manual gas limit'
+    const tx = await contract.transfer_test(30, ict_ca, { gasLimit: 100000 });
 
     
     const receipt = await tx.wait();
